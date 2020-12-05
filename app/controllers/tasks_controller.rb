a757cf6afbf0823b1903dd46af79a7a946366317
yuki.inoue <git.y.inoue@gmail.com>
2020-12-05 07:19:59 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -23,13 +24,10 @@ class TasksController < ApplicationController
     end
   end
 
-  def edit
-    @task = Task.find(params[:id])
+  def edit 
   end
 
-  def update
-    @task = Task.find(params[:id])
-    
+  def update    
     if @task.update(task_params)
       flash[:success] = "タスクは正常に更新されました"
       redirect_to @task
@@ -39,8 +37,7 @@ class TasksController < ApplicationController
     end
   end
 
-  def destroy
-    @task = Task.find(params[:id])
+  def destroy    
     @task.destroy
     
     flash[:success] = "タスクは正常に削除されました"
@@ -48,6 +45,10 @@ class TasksController < ApplicationController
   end
   
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
   
   def task_params
     params.require(:task).permit(:content)
